64be78d8ff701b8b1506cc205d7002dade990e4f
Jack <Jwenschhof@bsd220.org>
2022-09-28 09:34:30 -0500

    Initial Commit

---
+++ Foo Bar 0/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Foo Bar 0
+//
+//  Created by Jack Wenschhof on 8/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var counter = 0
+    @State private var display = "0"
+    @State private var color = Color.black
+    
+    var body: some View {
+        ZStack {
+            color.ignoresSafeArea()
+            Text(display)
+                .font(.system(size:128))
+                .multilineTextAlignment(.center)
+                .preferredColorScheme(.dark)
+                .onTapGesture {
+                    counter += 1
+                    if counter % 3 == 0 && counter % 5 == 0 {
+                        display = "Foo Bar"
+                        color = Color.red
+                    } else if counter % 3 == 0 {
+                        display = "Foo"
+                        color = Color.green
+                    } else if counter % 5 == 0 {
+                        display = "Bar"
+                        color = Color.yellow
+                    }
+                    
+                    
+                    else {
+                        
+                        display = String(counter)
+                    }
+                    
+                }
+                .onLongPressGesture {
+                    counter = 0
+                    display = "0"
+                    color = Color.black
+                }
+                .padding()
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Foo Bar 0/Foo_Bar_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_Bar_0App.swift
+//  Foo Bar 0
+//
+//  Created by Jack Wenschhof on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Foo_Bar_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
